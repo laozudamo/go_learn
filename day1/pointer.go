221d2e24bbd5825c69feeb5a378c68f31479f399
laozudamo <laozudamo@gmail.com>
2022-12-15 00:39:07 +0800

    feat: go学习

--- day1/pointer.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var value string = "hello world"
-	var point *string = &value
-	fmt.Println(point, *point)
-}
--- day1/var.go
+++ day1/var.go
@@ -2,15 +2,32 @@ package main
 
 import "fmt"
 
+// 指定变量类型
+
 func main() {
-	fmt.Println("你好,你是个狗狗")
+	var num1 int = 10
 
-	// 打包声明多个变量
+	// 不同变量相同类型的声明
+	var num2, num3 int = 20, 8
+	fmt.Println(num1, num2, num3)
+
+	// 多个变量不同类型的声明
 	var (
-		age = 24
-		sex = "男"
+		a int    = 10
+		b string = "hahha"
 	)
+	fmt.Println(a, b)
 
-	fmt.Println(age, sex)
+	// 采用类型推断
+	x := "已知数"
+	y := 999
 
+	fmt.Println(x, y)
 }
+
+// 类型推断声明
+// func main() {
+// 	num1 := 1
+// 	num2 := 2
+// 	fmt.Println(num1, num2)
+// }
