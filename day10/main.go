2be60ea2db82a713b9b7e3def293703681db4646
laozudamo <laozudamo@gmail.com>
2023-02-24 00:20:56 +0800

    feat: 连接数据库

---
+++ day10/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var SqlDB *sql.DB
+
+func main() {
+	initData()
+	queryData()
+}
+
+func initDB() {
+	var err error
+	SqlDB, err = sql.Open("mysql", "root:root123456@/gp_learn")
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Printf("\"成功\": %v\n", "成功")
+	}
+	// See "Important settings" section.
+	SqlDB.SetConnMaxLifetime(time.Minute * 3)
+	SqlDB.SetMaxOpenConns(10)
+	SqlDB.SetMaxIdleConns(10)
+}
+
+func init() {
+	initDB()
+}
+
+func initData() {
+	s := "insert into login_info(user,pwd) values(?, ?)"
+	r, err := SqlDB.Exec(s, "jackkk", "1212121")
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
+	fmt.Printf("r: %v\n", r)
+}
+
+type User struct {
+	user string
+	pwd  string
+	id   int
+}
+
+func queryData() {
+	var u User
+	s := "select * from login_info where id = ?"
+	err := SqlDB.QueryRow(s, 1).Scan(&u.pwd, &u.user, &u.id)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+	} else {
+		fmt.Printf("u: %V\n", u.id, u.pwd, u.user)
+	}
+}
+
+func queryManyRow() {
+
+}
